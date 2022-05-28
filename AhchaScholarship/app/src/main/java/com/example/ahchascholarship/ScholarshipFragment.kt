6cb57cd3ca1a3fe3f1c2d5a63be10999498f7291
Lisiant <wkdxod321@gmail.com>
2022-05-28 12:31:04 +0900

    2022.05.28 12:28 즐겨찾기 버튼 클릭 시 데이터베이스에 반영하려고 했으나 앱을 종료하고 재실행하거나, 다른 탭으로 이동했을 경우 반영 안되는 문제 발생

--- AhchaScholarship/app/src/main/java/com/example/ahchascholarship/ScholarshipFragment.kt
+++ AhchaScholarship/app/src/main/java/com/example/ahchascholarship/ScholarshipFragment.kt
@@ -1,29 +1,37 @@
 package com.example.ahchascholarship
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ahchascholarship.databinding.FragmentScholarshipBinding
 
 class ScholarshipFragment : Fragment() {
     lateinit var binding: FragmentScholarshipBinding
+    lateinit var db: ScholarshipDBHelper
+    var scholarshipDataList = ArrayList<ScholarshipData>()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        db = ScholarshipDBHelper(context)
+        scholarshipDataList = db.getAllRecord()
+    }
 
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentScholarshipBinding.inflate(inflater, container, false)
-        // inputDummyData()
-        val database = ScholarshipDBHelper(context)
-        val scholarshipDataList = database.getAllRecord()
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val scholarshipRVAdapter = ScholarshipRVAdapter(scholarshipDataList)
         binding.scholarshipMainRv.adapter = scholarshipRVAdapter
@@ -31,18 +39,28 @@ class ScholarshipFragment : Fragment() {
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
 
-        scholarshipRVAdapter.setScholarshipItemClickListener(object : ScholarshipRVAdapter.OnItemClickListener {
+        scholarshipRVAdapter.setScholarshipItemClickListener(object :
+            ScholarshipRVAdapter.OnItemClickListener {
             override fun onItemClick(scholarshipData: ScholarshipData) {
-                Log.d("DATA_CLICKED", "true")
+                Log.d("DATA_CLICKED", scholarshipData.상품명.toString() + scholarshipData.favorite.toString())
             }
 
-            override fun applyFavorite(position: Int) {
-                Log.d("FAVORITE_CLICKED", "true")
+            override fun clickFavorite(scholarshipData: ScholarshipData, position: Int) {
+                scholarshipData.favorite = !scholarshipData.favorite
+                /*
+                 *  updateFavorite 사용 시 문제 발생:
+                 *  즐겨찾기 추가 후 앱을 종료하고 다시 켰을 때
+                 *  or BottomNavigation 이동(대외활동, MY) 후 다시 장학금 탭으로 돌아왔을 때
+                 *  해당 내용이 저장이 안됨.
+                 */
+                db.setFavorite(10)
+                scholarshipRVAdapter.notifyDataSetChanged()
+                Log.d("Favorite_CLICKED", scholarshipData.favorite.toString())
 
             }
         })
 
-        return binding.root
+
     }
 
 
